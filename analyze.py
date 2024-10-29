6ed3e6f3e45779db41da678f32db4f3a5250281e
robertogreco99 <s310161@studenti.polito.it>
2024-10-29 10:36:05 +0100

    csv unico

--- analyze.py
+++ analyze.py
@@ -6,9 +6,11 @@ import sys
 from scipy.stats import gmean
 import os  
 
+# Initialize an empty list to hold all metrics results
+all_metrics_results = []
 
 if len(sys.argv) != 11:
-    print("Error , format is : python3 analyze.py <dataset> <width> <height> <bitrate> <video_codec> <model_version> <output_directory> <original_video> <width_old> <height_old>")
+    print("Error, format is : python3 analyze.py <dataset> <width> <height> <bitrate> <video_codec> <model_version> <output_directory> <original_video> <width_old> <height_old>")
     sys.exit(1)
 
 # Parameters
@@ -25,27 +27,25 @@ height_old = sys.argv[10]
 
 print(f"width_old: {width_old}, height_old: {height_old}")
 
-
+# Directory for graph results
 if width_old != '1920' or height_old != '1080':
- graph_directory = os.path.join(
-    output_directory,
-    dataset,
-    original_video,
-    f"{dataset}__{width_old}x{height_old}__{bitrate}_{video_codec}__{model_version}_resized_{width}x{height}"
-)
+    graph_directory = os.path.join(
+        output_directory,
+        dataset,
+        original_video,
+        f"{dataset}__{width_old}x{height_old}__{bitrate}_{video_codec}__{model_version}_resized_{width}x{height}"
+    )
 else:
- graph_directory = os.path.join(
-    output_directory,
-    dataset,
-    original_video,
-    f"{dataset}__{width_old}x{height_old}__{bitrate}_{video_codec}__{model_version}"
- )
-
+    graph_directory = os.path.join(
+        output_directory,
+        dataset,
+        original_video,
+        f"{dataset}__{width_old}x{height_old}__{bitrate}_{video_codec}__{model_version}"
+    )
 
 os.makedirs(graph_directory, exist_ok=True)
 
-# Create the json path name
-
+# Create the JSON path name
 if width_old != '1920' or height_old != '1080':
     json_filename = f'/results/result__{dataset}__{width_old}x{height_old}__{bitrate}__{video_codec}__{model_version}_resized_{width}x{height}.json'
 else:
@@ -54,7 +54,7 @@ else:
 # Print json filename
 print(f"Json file path: {json_filename}")
 
-# Read the 
+# Read the JSON file
 with open(json_filename) as f:
     data = json.load(f)
 
@@ -69,7 +69,7 @@ for frame in frames_list:
     row.update(metrics)
     frames_rows.append(row)
 
-# A new dataframe
+# Create a DataFrame for current file's metrics
 dframes = pd.DataFrame(frames_rows)
 print(dframes)
 
@@ -100,7 +100,7 @@ def calculate_metrics(column_name):
         total_variation = np.mean(abs_diff_scores)
         print(f"Total variation {column_name}: {total_variation}")
 
-        #norms
+        # Norms
         norm_lp_1 = np.power(np.mean(np.power(np.array(dframes[column_name]), 1)), 1.0 / 1)  # Norm L_1
         print(f"Norm L_1 of {column_name} values is: {norm_lp_1}")
 
@@ -110,7 +110,7 @@ def calculate_metrics(column_name):
         norm_lp_3 = np.power(np.mean(np.power(np.array(dframes[column_name]), 3)), 1.0 / 3)  # Norm L_3
         print(f"Norm L_3 of {column_name} values is: {norm_lp_3}")
 
-        return mean_value, harmonic_mean_value, geometric_mean_value, total_variation, norm_lp_1,norm_lp_2,norm_lp_3
+        return mean_value, harmonic_mean_value, geometric_mean_value, total_variation, norm_lp_1, norm_lp_2, norm_lp_3
     else:
         print(f"Metric '{column_name}' not found in the DataFrame.")
         return None
@@ -140,46 +140,19 @@ for metric in metrics_to_evaluate:
 print("METRICS RESULTS")
 print(metrics_results)
 
-# Create scatter plots for each metric
-def create_scatter_plot(x, y, x_label, y_label, title, output_img):
-    plt.scatter(x, y, color='blue')
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
-    plt.grid(True)
-    plt.savefig(output_img)
-    plt.close()
-    print(f"Plot saved as {output_img}")
-
-# Scatter plots for metrics
+# Save metrics results for the current file into the global list
 for metric, values in metrics_results.items():
-    mean_value, harmonic_mean_value, geometric_mean_value, total_variation, norm_lp_1,norm_lp_2,norm_lp_3 = values
-    create_scatter_plot(mean_value, harmonic_mean_value, 'Mean', 'Harmonic Mean',
-                     f'{metric} Mean vs {metric} Harmonic Mean',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_harmonic_mean.png'))
-
-    create_scatter_plot(mean_value, geometric_mean_value, 'Mean', 'Geometric Mean',
-                     f'{metric} Mean vs {metric} Geometric Mean',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_geometric_mean.png'))
-
-    create_scatter_plot(mean_value, total_variation, 'Mean', 'Total Variation',
-                     f'{metric} Mean vs {metric} Total Variation',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_total_variation.png'))
-    create_scatter_plot(mean_value, norm_lp_1, 'Mean', f'Norm L_{1}',
-                     f'{metric} Mean vs Norm L_{1}',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_lp_norm.png'))    
-    create_scatter_plot(mean_value, norm_lp_2, 'Mean', f'Norm L_{2}',
-                     f'{metric} Mean vs Norm L_{2}',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_lp_norm_2.png'))
-    create_scatter_plot(mean_value, norm_lp_3, 'Mean', f'Norm L_{3}',
-                     f'{metric} Mean vs Norm L_{3}',
-                     os.path.join(graph_directory, f'{metric}_mean_vs_lp_norm_3.png'))        
-
-# Save the metrics to CSV
-rows = []
-for metric, values in metrics_results.items():
-    mean_value, harmonic_mean_value, geometric_mean_value, total_variation, norm_lp_1,norm_lp_2,norm_lp_3 = values
-    rows.append({
+    mean_value, harmonic_mean_value, geometric_mean_value, total_variation, norm_lp_1, norm_lp_2, norm_lp_3 = values
+    all_metrics_results.append({
+        "Dataset": dataset,
+        "Original file name " : original_video,
+        "Width original" : width_old,
+        "Height original": height_old,
+        "Width": width,
+        "Height": height,
+        "Bitrate": bitrate,
+        "Video Codec": video_codec,
+        "Model Version": model_version,
         "Metric": metric,  
         "Mean": mean_value,
         "Harmonic Mean": harmonic_mean_value,
@@ -190,9 +163,16 @@ for metric, values in metrics_results.items():
         "Norm L_3": norm_lp_3
     })
 
-# Create a DataFrame and save it as CSV
-df_metrics = pd.DataFrame(rows)
-csv_filename = f'/results/result__{dataset}__{width_old}x{height_old}__{bitrate}__{video_codec}__{model_version}.json_resized_{width}x{height}.csv'
-df_metrics.to_csv(csv_filename, index=False)
+# Create a DataFrame for all metrics and save it as a CSV
+df_all_metrics = pd.DataFrame(all_metrics_results)
+csv_filename = f'/results/combined_results.csv'
 
-print("Csv ending")
+# Append to CSV without writing the header if the file already exists
+if os.path.isfile(csv_filename):
+    # If the file exists, append the new data
+    df_all_metrics.to_csv(csv_filename, mode='a', header=False, index=False)
+else:
+    # If the file does not exist, create it with header
+    df_all_metrics.to_csv(csv_filename, mode='w', header=True, index=False)
+
+print("Combined CSV updated.")
