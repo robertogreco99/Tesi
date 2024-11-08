59fd37ed387b0db97149f4fa95c2a51e344a8117
robertogreco99 <s310161@studenti.polito.it>
2024-11-08 17:18:39 +0100

    experiment on KUGDV csgo sourcr

--- Simulation/KUGDV/run_experiments_KUGDV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_file="/home/greco/home/docker/Result/commands.txt"
-input_file1=/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Result/commands.txt
-while IFS= read -r command; do
-    if [[ -n "$command" ]]; then
-        eval "$command"
-    fi
-done < "$input_file1"
