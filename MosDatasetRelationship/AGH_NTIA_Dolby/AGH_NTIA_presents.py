fc8dd1b03c2ef392673e1b5da54e370c3da403b2
robertogreco99 <s310161@studenti.polito.it>
2024-11-15 10:59:27 +0100

    tutti i file di dolby hanno il mos

--- MosDatasetRelationship/AGH_NTIA_Dolby/AGH_NTIA_presents.py
+++ MosDatasetRelationship/AGH_NTIA_Dolby/AGH_NTIA_presents.py
@@ -1,7 +1,7 @@
 import json
 
-input_file = './ScoresKUGVD.json'
-output_file = './AllKUGDVwithScores.txt'
+input_file = '/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Mos/ScoresAGH_NTIA_Dolby.json'
+output_file = './AllScoresAGH_NTIA_Dolby.txt'
 
 with open(input_file, 'r') as f:
     data = json.load(f)
@@ -9,6 +9,6 @@ with open(input_file, 'r') as f:
 with open(output_file, 'w') as f:
     for entry in data.get('scores', []):
         if 'PVS' in entry and 'PVS_ID' in entry['PVS']:
-            f.write(entry['PVS']['PVS_ID'] + '\n')
+            f.write(entry['PVS']['PVS_ID'] + '.y4m\n')
 
 print(f"PVS_ID values have been extracted to {output_file}")
