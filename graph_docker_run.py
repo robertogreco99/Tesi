cb61efab46edea20d9c8d6fc6093c221c0019679
robertogreco99 <s310161@studenti.polito.it>
2024-11-23 17:53:24 +0100

    graph_script.py

---
+++ graph_docker_run.py
@@ -0,0 +1,10 @@
+import subprocess
+dataset = "KUGVD"
+command=f"podman run --rm -it -v /home/greco/home/docker/Result:/results  python3 graph_script.py {dataset}"
+if command:
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+            print(f"Error in the execution of command: {command}")
+            print(e)
+      
