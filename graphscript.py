e1503852b3c08c26d635aaf6a8eb087cb98fa4e3
robertogreco99 <s310161@studenti.polito.it>
2024-11-23 17:11:37 +0100

    pvs

--- graphscript.py
+++ graphscript.py
@@ -23,6 +23,8 @@ else:
         "psnr_hvs_cb", "vmaf_float_b_v0.6.3_bagging",
         "vmaf_b_v0.6.3_bagging", 
         ]
+    temporal_pooling_values = ["mean", "harmonic_mean", "geometric_mean", "total_variation", "norm_lp1", "norm_lp2", "norm_lp3"]
+    temporal_pooling_graph = ["mean", "harmonic_mean", "geometric_mean","norm_lp1", "norm_lp2", "norm_lp3"]
 
     lo_column_float_b = "vmaf_float_b_v0.6.3_ci_p95_lo"
     hi_column_float_b = "vmaf_float_b_v0.6.3_ci_p95_hi"
@@ -40,9 +42,12 @@ else:
     vmaf_output_path = os.path.join(output_path, "VMAF_Models")
     features_output_path = os.path.join(output_path, "Features")
     hi_lo_output_path = os.path.join(output_path,"HI_LO")
+    pvs_path=os.path.join(output_path,"PVS")
     os.makedirs(vmaf_output_path, exist_ok=True)
     os.makedirs(features_output_path, exist_ok=True)
     os.makedirs(hi_lo_output_path, exist_ok=True)
+    os.makedirs(pvs_path, exist_ok=True)
+
 
 
     # Extract unique values for the graphs
@@ -53,21 +58,25 @@ else:
     vmaf_float_b_values = data['vmaf_float_b_v0.6.3'].unique()
     vmaf_b_values = data['vmaf_b_v0.6.3'].unique()
 
+    # Unique pvs
+    pvs=data['Distorted_file_name'].unique()
 
     colors_video_codec = plt.cm.tab10(np.linspace(0, 1, len(video_codecs)))
     colors_FPS = plt.cm.viridis(np.linspace(0, 1, len(FPS_values)))
     colors_Duration = plt.cm.plasma(np.linspace(0, 1, len(Duration_values)))
     colors_bitrate = plt.cm.cool(np.linspace(0, 1, len(bitrate_values)))
+    colors_temporal_pooling=plt.cm.twilight(np.linspace(0,1,len(temporal_pooling_graph)))
 
     # Create dictionary to map the colors
     codec_color_map = {codec: colors_video_codec[i] for i, codec in enumerate(video_codecs)}
     FPS_color_map = {FPS: colors_FPS[i] for i, FPS in enumerate(FPS_values)}
     Duration_color_map = {Duration: colors_Duration[i] for i, Duration in enumerate(Duration_values)}
     bitrate_color_map = {bitrate: colors_bitrate[i] for i, bitrate in enumerate(bitrate_values)}
+    temporal_pooling_color_map={temporal_pooling : colors_temporal_pooling[i] for i ,temporal_pooling in enumerate(temporal_pooling_graph)}
 
-    temporal_pooling_values = ["mean", "harmonic_mean", "geometric_mean", "total_variation", "norm_lp1", "norm_lp2", "norm_lp3"]
 
     # Per every temporal pooling create different graph
+    """
     for temporal_pooling_value in temporal_pooling_values:
         filtered_data = data[data['temporal_pooling'] == temporal_pooling_value]
 
@@ -141,7 +150,7 @@ else:
         else :
             print("There are only similar fps values")
         # Duration
-        """
+        
         for y_column in vmaf_models + features:
             if y_column not in filtered_data.columns:
                 print(f"Columns {y_column} not found in {dataset}")
@@ -170,10 +179,8 @@ else:
             plt.savefig(output_file, bbox_inches='tight')
             print(f"Graph saved: {output_file}")
             plt.close()
-            """
-        
-       
-
+            
+        # float b model
         if temporal_pooling_value !="total_variation":
             if lo_column_float_b not in filtered_data.columns or hi_column_float_b not in filtered_data.columns:
                print(f"Columns {lo_column_float_b} or {hi_column_float_b} not found in {dataset}")
@@ -217,10 +224,7 @@ else:
                 plt.savefig(output_file, bbox_inches='tight')
                 print(f"Graph saved: {output_file}")
                 plt.close()
-
-
-       
-        
+        # b model
         if temporal_pooling_value !="total_variation":
             if lo_column_b not in filtered_data.columns or hi_column_b not in filtered_data.columns:
                 print(f"Columns {lo_column_b} or {hi_column_b} not found in {dataset}")
@@ -294,3 +298,55 @@ else:
             plt.savefig(output_file, bbox_inches='tight')
             print(f"Graph saved: {output_file}")
             plt.close()
+"""  
+    for pvs_value in pvs:
+        output_dir = f"{pvs_path}/{pvs_value}"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        filtered_data = data[data['Distorted_file_name'] == pvs_value]
+
+        if filtered_data.empty:
+            print(f"No data found for the pvs sequence: {pvs_value} in dataset {dataset}")
+            continue
+
+        x_value = filtered_data['MOS'].values[0]
+
+        for feature in features:
+            plt.figure(figsize=(10, 6))
+            x_values = []
+            y_values = []
+            labels = []
+            colors = []
+            for temporal_pooling_value in temporal_pooling_graph:
+                if temporal_pooling_value == "total_aggregation":
+                    continue   
+                temporal_filtered_data = filtered_data[filtered_data['temporal_pooling'] == temporal_pooling_value]
+                if temporal_filtered_data.empty:
+                    print(f"No data found for temporal pooling {temporal_pooling_value} in {pvs_value}")
+                    continue
+                feature_value = temporal_filtered_data[feature].values
+                x_values.append(x_value)  
+                y_values.append(feature_value[0])  
+                labels.append(f"{temporal_pooling_value}")
+                colors.append(temporal_pooling_color_map[temporal_pooling_value])  
+             # Aggiungi scatter per ogni metodo di pooling
+            print(f"x value : {x_value}, y value : {y_values}")
+
+            for i, temporal_pooling_value in enumerate(temporal_pooling_graph):
+                if temporal_pooling_value == "total_aggregation":
+                    continue
+                plt.scatter(x_values[i], y_values[i], label=labels[i], color=colors[i], marker='o', alpha=0.7) 
+            #plt.scatter(x_values, y_values, label=labels, color=features_color_map[feature], marker='o', alpha=0.7) 
+            plt.title(f"{x_column} vs {feature} - feature:{feature}")
+            plt.xlabel(x_column)
+            plt.ylabel(feature)
+            plt.grid(True)
+            plt.legend(title="Temporal Pooling", loc='center left', bbox_to_anchor=(1, 0.5))
+
+            output_file = f"{pvs_path}/{pvs_value}/scatter_{feature}.png"
+            plt.savefig(output_file, bbox_inches='tight')
+            print(f"Graph saved: {output_file}")
+            plt.close()
+    
+    
