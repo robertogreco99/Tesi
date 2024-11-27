ba94fdb08ab684c86839a495324f81fa0d5deb1e
robertogreco99 <s310161@studenti.polito.it>
2024-11-27 11:48:00 +0100

    modifica per il file per più databse

--- run_more_vmaf_simulation.py
+++ run_more_vmaf_simulation.py
@@ -3,8 +3,8 @@ import subprocess
 database1 = 'AVT-VQDB-UHD-1_2'
 database2 = 'AVT-VQDB-UHD-1_3'
 
-file_path1 = f'/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Result/commands_{database1}.txt'
-file_path2 = f'/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Result/commands_{database2}.txt'
+file_path1 = f'/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Result/{database1}/commands_{database1}.txt'
+file_path2 = f'/home/roberto/Scaricati/Tesi/Lavorosullatesi/Tesi/Result/{database2}/commands_{database2}.txt'
 
 def run_commands_from_file(file_path):
     try:
